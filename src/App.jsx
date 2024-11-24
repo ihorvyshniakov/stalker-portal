ada963a752fa76d5f0d2fba6c90179472e9dfa33
Vyshniakov Ihor <ihor.vyshniakov@qlicks.nl>
2024-11-24 22:55:08 +0100

    added Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Button, Container } from '@mui/material'
 import './App.scss'
+
+import Header from './components/Header'
 
 function App() {
 	return (
-		<Container maxWidth="sm">
-			<h1>Stalker 2 - Portal</h1>
-			<Button variant="outlined">Hello world</Button>
-		</Container>
+		<>
+			<Header />
+		</>
 	)
 }
 
