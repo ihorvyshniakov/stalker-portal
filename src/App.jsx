21abda9e008f868fe220f5d08d3453c582bc3c40
Vyshniakov Ihor <ihor.vyshniakov@qlicks.nl>
2024-11-24 20:24:56 +0100

    added normalize styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Button } from '@mui/material'
+import { Button, Container } from '@mui/material'
 import './App.scss'
 
 function App() {
 	return (
-		<>
+		<Container maxWidth="sm">
 			<h1>Stalker 2 - Portal</h1>
 			<Button variant="outlined">Hello world</Button>
-		</>
+		</Container>
 	)
 }
 
