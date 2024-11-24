1912ecd5665069b9eea739659a70c287250aa9b6
Vyshniakov Ihor <ihor.vyshniakov@qlicks.nl>
2024-11-24 20:15:42 +0100

    installed: Material-UI, Icons + font 'Roboto'

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Button } from '@mui/material'
 import './App.scss'
 
 function App() {
 	return (
 		<>
 			<h1>Stalker 2 - Portal</h1>
+			<Button variant="outlined">Hello world</Button>
 		</>
 	)
 }
