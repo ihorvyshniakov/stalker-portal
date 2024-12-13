c5e095abf458c13fdd661640fea8bdb6c55738de
Vyshniakov Ihor <gramazekavip@gmail.com>
2024-12-13 11:43:52 +0100

    fixed error with routing, added base URL

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import './App.scss'
 import Header from './components/Header'
 import Content from './components/Content'
 
+// TODO: fix react-router
+// TODO: deploy correctly to Github pages
+// https://game8.co/games/STALKER-2-Heart-of-Chornobyl/archives/485907
+
 function App() {
 	return (
 		<>
